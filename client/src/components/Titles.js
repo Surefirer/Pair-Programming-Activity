6069efb694fea510d5efbbca863bccf0c8d73e2d
Steven Martins <Steven@MacBook-Pro.local>
2020-06-05 14:10:19 -0700

    commiting client folder to repo

---
+++ client/src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">What's The Weather Like?</h1>
+    <h3 className="title-container__subtitle">
+      Don't feel like going outside? Find out the weather in your city here!
+    </h3>
+  </div>
+);
+
+export default Titles;
